59fcc564297810ca706db076088014931e50907b
Bram <b93ram@gmail.com>
2023-09-20 19:11:10 -0400

    testing category endpoint changed port in application properies

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,11 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users cascade;
+DROP TABLE IF EXISTS categories cascade;
+DROP TABLE IF EXISTS quizzes cascade;
+DROP TABLE IF EXISTS quiz_questions cascade;
+DROP TABLE IF EXISTS users_quizzes cascade;
+
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -23,12 +28,20 @@ CREATE TABLE quizzes (
     CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(category_id)
 );
 
-CREATE TABLE users_quizzes (
-    user_id INTEGER,
+CREATE TABLE quiz_questions (
+    question_id SERIAL PRIMARY KEY,
     quiz_id INTEGER,
-    PRIMARY KEY (user_id, quiz_id),
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+    question VARCHAR(250) NOT NULL,
+    answer VARCHAR(250) NOT NULL,
     CONSTRAINT fk_quiz FOREIGN KEY (quiz_id) REFERENCES quizzes(quiz_id)
 );
 
+CREATE TABLE users_quizzes (
+    user_id INTEGER,
+    quiz_id SERIAL PRIMARY KEY,
+    question_id INTEGER,
+    answer_id SERIAL, -- This should be SERIAL for unique identifier
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT fk_question FOREIGN KEY (question_id) REFERENCES quiz_questions(question_id)
+);
 COMMIT TRANSACTION;
